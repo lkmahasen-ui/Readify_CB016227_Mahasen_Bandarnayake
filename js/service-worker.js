83191186a6224fe2ff13a9aa509b6d613d929527
lkmahasen-ui <laksandamahasen@gmail.com>
2026-02-09 15:06:28 +0530

    Update cache URLs to include project path

--- js/service-worker.js
+++ js/service-worker.js
@@ -3,34 +3,34 @@
 const CACHE_NAME = 'readify-cache-v1';
 const urlsToCache = [
   // html
-  './',
-  'index.html',
-  'explorer.html',
-  'feedback.html',
-  'flow.html',
-  'progress.html',
-  'recommender.html',
+  '/Readify_Mahasen_Bandarnayake/',
+  '/Readify_Mahasen_Bandarnayake/index.html',
+  '/Readify_Mahasen_Bandarnayake/explorer.html',
+  '/Readify_Mahasen_Bandarnayake/feedback.html',
+  '/Readify_Mahasen_Bandarnayake/flow.html',
+  '/Readify_Mahasen_Bandarnayake/progress.html',
+  '/Readify_Mahasen_Bandarnayake/recommender.html',
 
   // css
-  'css/navbar.css',
-  'css/index.css',
-  'css/footer.css',
-  'css/loading.css',
-  'css/explorer.css',
-  'css/feedback.css',
-  'css/flow.css',
-  'css/progress.css',
-  'css/recommender.css',
+  '/Readify_Mahasen_Bandarnayake/css/navbar.css',
+  '/Readify_Mahasen_Bandarnayake/css/index.css',
+  '/Readify_Mahasen_Bandarnayake/css/footer.css',
+  '/Readify_Mahasen_Bandarnayake/css/loading.css',
+  '/Readify_Mahasen_Bandarnayake/css/explorer.css',
+  '/Readify_Mahasen_Bandarnayake/css/feedback.css',
+  '/Readify_Mahasen_Bandarnayake/css/flow.css',
+  '/Readify_Mahasen_Bandarnayake/css/progress.css',
+  '/Readify_Mahasen_Bandarnayake/css/recommender.css',
 
   // js
-  'js/index.js',
-  'js/explorer.js',
-  'js/feedback.js',
-  'js/flow.js',
-  'js/loading.js',
-  'js/recommender.js',
-  'js/reuse.js',
-  'js/tracker.js',
+  '/Readify_Mahasen_Bandarnayake/js/index.js',
+  '/Readify_Mahasen_Bandarnayake/js/explorer.js',
+  '/Readify_Mahasen_Bandarnayake/js/feedback.js',
+  '/Readify_Mahasen_Bandarnayake/js/flow.js',
+  '/Readify_Mahasen_Bandarnayake/js/loading.js',
+  '/Readify_Mahasen_Bandarnayake/js/recommender.js',
+  '/Readify_Mahasen_Bandarnayake/js/reuse.js',
+  '/Readify_Mahasen_Bandarnayake/js/tracker.js',
 
   // Favicon & manifest
   '/Readify_Mahasen_Bandarnayake/assets/images/favicon/favicon-16x16.png',
@@ -44,16 +44,16 @@ const urlsToCache = [
   '/Readify_Mahasen_Bandarnayake/assets/images/favicon/site.webmanifest',
 
   // background images
-  'assets/images/Background/bookexplorer.jpg',
-  'assets/images/Background/bookhome.jpg',
-  'assets/images/Background/feedback.webp',
-  'assets/images/Background/recommender.webp',
-  'assets/images/Background/tracker.webp',
+  '/Readify_Mahasen_Bandarnayake/assets/images/Background/bookexplorer.jpg',
+  '/Readify_Mahasen_Bandarnayake/assets/images/Background/bookhome.jpg',
+  '/Readify_Mahasen_Bandarnayake/assets/images/Background/feedback.webp',
+  '/Readify_Mahasen_Bandarnayake/assets/images/Background/recommender.webp',
+  '/Readify_Mahasen_Bandarnayake/assets/images/Background/tracker.webp',
 
   // Audio 
-  'assets/audio/Magical-Moments-chosiccom.mp3',
-  'assets/audio/Morning-Routine-Lofi-Study-Music.mp3',
-  'assets/audio/RELAX-LOFI-chosiccom.mp3',
+  '/Readify_Mahasen_Bandarnayake/assets/audio/Magical-Moments-chosiccom.mp3',
+  '/Readify_Mahasen_Bandarnayake/assets/audio/Morning-Routine-Lofi-Study-Music.mp3',
+  '/Readify_Mahasen_Bandarnayake/assets/audio/RELAX-LOFI-chosiccom.mp3',
 ];
 
 self.addEventListener('install', event => {
